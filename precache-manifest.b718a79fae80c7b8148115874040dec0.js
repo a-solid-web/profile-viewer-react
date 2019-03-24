e2e34793ed3b29a44f1fa212d6db2e771e72e5b7
ludwigschubi <ludwigschubi1@gmail.com>
2019-03-24 19:48:35 +0100

    Updates

---
+++ precache-manifest.b718a79fae80c7b8148115874040dec0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "515de2a05d40ff396a7d",
+    "url": "/profile-viewer-react/static/css/main.1c488ee3.chunk.css"
+  },
+  {
+    "revision": "515de2a05d40ff396a7d",
+    "url": "/profile-viewer-react/static/js/main.b99816d5.chunk.js"
+  },
+  {
+    "revision": "60819b1766a87293175a",
+    "url": "/profile-viewer-react/static/js/runtime~main.2e4ba336.js"
+  },
+  {
+    "revision": "d9e8eace33f360bf842c",
+    "url": "/profile-viewer-react/static/js/2.949c0824.chunk.js"
+  },
+  {
+    "revision": "bc7e55e483e69fa9624ce8bae834e772",
+    "url": "/profile-viewer-react/index.html"
+  }
+];
