d91be42437a59e50a74f0531bfe19e497f97051e
ludwigschubi <ludwigschubi1@gmail.com>
2019-04-14 10:05:01 +0200

    Updates

---
+++ precache-manifest.bd751c7f6875ef7cfa36ea67a3f7ca6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2f83dc4704cd5bade86",
+    "url": "/profile-viewer-react/static/css/main.8cb75eb3.chunk.css"
+  },
+  {
+    "revision": "b2f83dc4704cd5bade86",
+    "url": "/profile-viewer-react/static/js/main.3e350c28.chunk.js"
+  },
+  {
+    "revision": "60819b1766a87293175a",
+    "url": "/profile-viewer-react/static/js/runtime~main.2e4ba336.js"
+  },
+  {
+    "revision": "5587bd546dc2afe1a5c1",
+    "url": "/profile-viewer-react/static/js/2.d18ecae9.chunk.js"
+  },
+  {
+    "revision": "099f0eb523573d80925e52fb4ccf76d8",
+    "url": "/profile-viewer-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/profile-viewer-react/precache-manifest.e6b64ec2509508feed22a8a2f27c7845.js"
+  "/profile-viewer-react/precache-manifest.bd751c7f6875ef7cfa36ea67a3f7ca6d.js"
 );
 
 workbox.clientsClaim();
